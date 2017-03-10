e8f30ffd9ba14d1de6aae073b1260892befd48f7
JackMarx <seriousfools@gmail.com>
2017-03-09 18:43:39 -0600

    started create action

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,4 +6,8 @@ class PagesController < ApplicationController
   def second
     
   end
+
+  def create
+    
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get '/' => 'pages#index'
   get '/second' => 'pages#second'
+  post '/pages' => 'pages#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
